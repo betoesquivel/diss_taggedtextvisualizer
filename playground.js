f9437d7d45e9defba3a53114fcbfc37b1f773290
Beto Esquivel <ppbeto94@gmail.com>
2016-08-02 13:50:20 +0100

    sets up reactjs playground

---
+++ playground.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './src/components/App.js';
+
+const container = document.createElement('div');
+document.body.appendChild(container);
+
+ReactDOM.render(<App />, container);
---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  render () {
+    return (
+      <div id="app">
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+
+}
+
+export default App;
