578e53a92c2a26bd6b1a839ff3be05637f1f6a84
Beto Esquivel <ppbeto94@gmail.com>
2016-08-10 11:34:59 +0100

    Adds proper visualization of tag info per different tagger/linker used.

---
+++ src/components/TagInfo.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import {Card} from 'belle';
+
+const style = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  alignContent: 'center',
+}
+
+class TagInfo extends React.Component {
+
+  constructor() {
+    super();
+    this.renderTag = this.renderTag.bind(this);
+  }
+
+  renderTag(tag) {
+    const {
+      'surface-form': stanfordSurfaceForm,
+      'spot': tagmeSurfaceForm,
+      'secTitle': wikifierSurfaceForm,
+    } = tag;
+
+    if (stanfordSurfaceForm) {
+      const {
+        type
+      } = tag;
+      return (
+        <div>
+          <h2> {stanfordSurfaceForm} </h2>
+          <h3> {type} </h3>
+        </div>
+      )
+    } else if (tagmeSurfaceForm) {
+      const {
+        'dbpedia_categories': categories
+      } = tag;
+      return (
+        <div>
+          <h2> {tagmeSurfaceForm} </h2>
+          <strong>Categories: <span> {categories.join()} </span></strong>
+          <p>
+            {
+              JSON.stringify({
+               'link_probability': tag.link_probability,
+               'rho': tag.rho,
+               'title': tag.title
+              })
+            }
+          </p>
+        </div>
+      );
+
+    } else if (wikifierSurfaceForm) {
+      const {
+        dbPediaIri,
+        'dbPediaTypes': categories
+      } = tag;
+      return (
+        <div>
+          <h2> {wikifierSurfaceForm} </h2>
+          <h3> {dbPediaIri} </h3>
+          <strong>Categories: <span> {categories.join()} </span></strong>
+          <p>
+            {
+              JSON.stringify({
+               'secTitle': tag.secTitle,
+               'secUrl': tag.secUrl,
+               'cosine': tag.cosine
+              })
+            }
+          </p>
+        </div>
+      );
+
+    }
+  }
+
+  render() {
+    const p = this.props;
+
+    return (
+      <div>
+        {
+          (p.color != 'white') ? 
+            <Card style={{ style, backgroundColor: p.color }}>
+              {this.renderTag(p.tag)}
+            </Card>
+          : ''
+        }
+      </div>
+    );
+  }
+}
+
+export default TagInfo;
